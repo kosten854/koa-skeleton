99e989f6f43e68132f7d4ccf4dfc45a9fd5864e0
exorcist <yag0da@yandex.ru>
2019-06-18 09:27:11 +0300

    init

---
+++ README.md
@@ -0,0 +1,61 @@
+  
+
+## Getting Started
+ 
+```zsh
+$ yarn
+$ yarn start
+```
+
+
+## Commands
+
+### Run
+
+```zsh
+# Run normally
+$ yarn start
+# Run the application with nodemon for development
+$ yarn dev
+```
+
+### Test
+
+```zsh
+# Test
+$ yarn test                           # Run all test
+$ yarn test:unit                      # Run only unit test
+$ yarn test:integration               # Run only integration test
+# Test (Watch Mode for development)
+$ yarn test:watch                     # Run all test with watch mode
+$ yarn test:watch:unit                # Run only unit test with watch mode
+$ yarn test:watch:integration         # Run only integration test with watch mode
+# Test Coverage
+$ yarn test:coverage                  # Calculate the coverage of all test
+$ yarn test:coverage:unit             # Calculate the coverage of unit test
+$ yarn test:coverage:integration      # Calculate the coverage of integration test
+# Test consistent coding style (Lint)
+$ yarn lint                           # Lint all sourcecode
+$ yarn lint:app                       # Lint app sourcecode
+$ yarn lint:test                      # Lint test sourcecode
+```
+
+### Archive
+
+```zsh
+$ yarn pack
+```
+
+
+## Test
+
+All test for this boilerplate uses following tools.
+
+- [Jest](https://github.com/facebook/jest)
+- [supertest](https://github.com/visionmedia/supertest): Easy HTTP assertions for integration test
+
+  
+
+## License
+ 
+ MIT
---
+++ app/app.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
+const logging = require('@kasa/koa-logging');
+const requestId = require('@kasa/koa-request-id');
+const apmMiddleware = require('./middlewares/apm');
+const errorHandler = require('./middlewares/errorHandler');
+const logger = require('./logger');
+const router = require('./routes');
+
+
+class App extends Koa {
+  constructor(...params) {
+    super(...params);
+
+    // Trust proxy
+    this.proxy = true;
+    // Disable `console.errors` except development env
+    this.silent = this.env !== 'development';
+
+    this.servers = [];
+
+    this._configureMiddlewares();
+    this._configureRoutes();
+  }
+
+  _configureMiddlewares() {
+    this.use(errorHandler());
+    this.use(apmMiddleware());
+    this.use(
+      bodyParser({
+        enableTypes: ['json', 'form'],
+        formLimit: '10mb',
+        jsonLimit: '10mb'
+      })
+    );
+    this.use(requestId());
+    this.use(logging({
+      logger,
+      overrideSerializers: false
+    }));
+    this.use(
+      cors({
+        origin: '*',
+        allowMethods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE', 'PATCH'],
+        allowHeaders: ['Content-Type', 'Authorization'],
+        exposeHeaders: ['Content-Length', 'Date', 'X-Request-Id']
+      })
+    );
+  }
+
+  _configureRoutes() {
+    // Bootstrap application router
+    this.use(router.routes());
+    this.use(router.allowedMethods());
+  }
+
+  listen(...args) {
+    const server = super.listen(...args);
+    this.servers.push(server);
+    return server;
+  }
+
+  async terminate() {
+    // TODO: Implement graceful shutdown with pending request counter
+    for (const server of this.servers) {
+      server.close();
+    }
+  }
+}
+
+module.exports = App;
